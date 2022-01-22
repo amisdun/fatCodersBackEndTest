b887ecd6962ce80b4cd510e6a1aaa4dd0f50c55f
amisdun <michealdunamis@gmail.com>
2022-01-22 19:34:24 +0000

    migration update to add new column to tables

---
+++ database/migrations/20220121181041-add_column_to_farm_building.js
@@ -0,0 +1,26 @@
+"use strict";
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    await queryInterface.addColumn("FarmBuildings", "farmUnitType", {
+      allowNull: false,
+      type: Sequelize.STRING,
+    });
+  },
+
+  async down(queryInterface) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+    await queryInterface.removeColumn("FarmBuildings", "farmUnitType");
+  },
+};
